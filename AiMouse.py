cb36d82a285b352b9593a04a828e2cdedd48fe2b
Sameer Gupta <sameergupta4873@gmail.com>
2022-12-13 22:40:10 +0530

    Update AiMouse.py

--- AiMouse.py
+++ AiMouse.py
@@ -1,69 +1,69 @@
 import cv2
-import numpy as np
-import HandTrackingModule as htm
 import time
 import autopy
+import HandTrackingModule as hmt
+import numpy as np
+import random
+import math
 
-##########################
+wScr, hScr = autopy.screen.size()
+
 wCam, hCam = 640, 480
-frameR = 100 # Frame Reduction
-smoothening = 7
-#########################
 
-pTime = 0
-plocX, plocY = 0, 0
+plocX , plocY = 0,0
 clocX, clocY = 0, 0
 
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-detector = htm.handDetector(maxHands=1)
-wScr, hScr = autopy.screen.size()
-# print(wScr, hScr)
+smoothening = 2
 
-while True:
-    # 1. Find hand Landmarks
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
-    # 2. Get the tip of the index and middle fingers
-    if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
 
-    # 3. Check which fingers are up
-    fingers = detector.fingersUp()
-    # print(fingers)
-    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),(255, 0, 255), 2)
-    # 4. Only Index Finger : Moving Mode
-    if fingers[1] == 1 and fingers[2] == 0:
-        # 5. Convert Coordinates
-        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-    # 6. Smoothen Values
-    clocX = plocX + (x3 - plocX) / smoothening
-    clocY = plocY + (y3 - plocY) / smoothening
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    cap.set(3, wCam)
+    cap.set(4, hCam)
+    detector = hmt.handleDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        
+        
+        
+        
+        
+        if len(lmList) != 0:
+            # print(lmList[4])
+            x = np.interp(lmList[4][1], (0, wCam), (0, wScr)) 
+            y = np.interp(lmList[4][2], (0, hCam), (0, hScr))
+            
+            fingers = detector.fingersUp()
+            
+            print(fingers)
+            
+            
+            clockX = plocX + (x - plocX) / smoothening
+            clockY = plocY + (y - plocY) / smoothening
+            
+            if fingers[1] == 1 and fingers[2] == 0:
+                autopy.mouse.move(wScr - clockX, clockY)
+            if fingers[1] == 1 and fingers[2] == 1:
+            # 9. Find distance between fingers
+                length, img, lineInfo = detector.findDistance(8, 12, img)
+                print(length)
+            # 10. Click mouse if distance short
+                if length < 40:
+                    cv2.circle(img, (lineInfo[4], lineInfo[5]),15, (0, 255, 0), cv2.FILLED)
+                    autopy.mouse.click()
+            
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
 
-    # 7. Move Mouse
-    autopy.mouse.move(wScr - clocX, clocY)
-    cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-    plocX, plocY = clocX, clocY
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-    # 8. Both Index and middle fingers are up : Clicking Mode
-    if fingers[1] == 1 and fingers[2] == 1:
-    # 9. Find distance between fingers
-        length, img, lineInfo = detector.findDistance(8, 12, img)
-        print(length)
-    # 10. Click mouse if distance short
-    if length < 40:
-        cv2.circle(img, (lineInfo[4], lineInfo[5]),15, (0, 255, 0), cv2.FILLED)
-        autopy.mouse.click()
+        cv2.imshow("Img", img)
+        cv2.waitKey(1)
 
-    # 11. Frame Rate
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 0), 3)
-    # 12. Display
-    cv2.imshow("image", img)
-    cv2.waitKey(1)
+if __name__ == '__main__' :
+    main()
